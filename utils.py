810b93e0db24b42b6990d69b0921efa9bb48198b
justarationalist <justarationalist@gmail.com>
2025-06-24 20:52:39 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 # from Learning_ML_utils.utils import decay_rate, print_weight_counts, print_weight_counts, DataTracker, EarlyStopping, GraphEntryData, display_history
 
 import torch
-print(torch.Tensor)
+
 def decay_rate(decay, per):
     return decay ** (1/per)
 
@@ -90,13 +90,13 @@ class EarlyStopping:
     def epoch_step(self):
         self.data_tracker.record(EarlyStopping.PATIENCE_SUM, self.stop_counter)
     def last_test_loss_ema(self):
-        return self.data_tracker[EarlyStopping.EMANAME][-1]
+        return self.data_tracker.get_data(EarlyStopping.EMANAME)[-1]
     def last_test_loss_diff_ema(self):
-        return self.data_tracker[EarlyStopping.EMADNAME][-1]
+        return self.data_tracker.get_data(EarlyStopping.EMADNAME)[-1]
     def last_patience_counter_diff(self):
-        return self.data_tracker[EarlyStopping.PATIENCE][-1]
+        return self.data_tracker.get_data(EarlyStopping.PATIENCE)[-1]
     def last_patience_counter(self):
-        return self.data_tracker[EarlyStopping.PATIENCE_SUM][-1]
+        return self.data_tracker.get_data(EarlyStopping.PATIENCE_SUM)[-1]
 
 def print_weight_counts(model):
     total = 0
