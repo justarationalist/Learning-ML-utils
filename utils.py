b43590d9f43a02777e77b618fc876069c9d2d0f1
justarationalist <justarationalist@gmail.com>
2025-06-24 20:33:54 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 # from Learning_ML_utils.utils import decay_rate, print_weight_counts, print_weight_counts, DataTracker, EarlyStopping, GraphEntryData, display_history
 
 import Learning_ML_utils.config as conf
-from Learning_ML_utils.config import *
 
 def decay_rate(decay, per):
     return decay ** (1/per)
